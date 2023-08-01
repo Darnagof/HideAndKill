ba95c5f039628ecb7139c6cc9861f47b88723f2d
Darnagof <gael.edern@orange.fr>
2023-08-01 22:50:24 +0200

    blank project

---
+++ Source/HideAndKill/HideAndKillGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HideAndKillGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HIDEANDKILL_API AHideAndKillGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
