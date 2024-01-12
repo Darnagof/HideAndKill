83fdf99d0428884262709180f88396c04309662b
xavzzzz <xavier.nicolle1@gmail.com>
2024-01-12 12:12:49 +0100

    Merge branch 'main' of https://github.com/Darnagof/HideAndKill

--- Source/HideAndKill/TP_ThirdPerson/TP_ThirdPersonCharacter.h
+++ Source/HideAndKill/TP_ThirdPerson/TP_ThirdPersonCharacter.h
@@ -59,10 +59,6 @@ class ATP_ThirdPersonCharacter : public ABaseCharacter
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Input, meta = (AllowPrivateAccess = "true"))
 	bool IsFreelooking;
 
-	//Dictates if player can QD state
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Input, meta = (AllowPrivateAccess = "true"))
-	bool CanTurn;
-
 public:
 	ATP_ThirdPersonCharacter();
 
